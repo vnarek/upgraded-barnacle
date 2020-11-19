af4d6ee81d9e9edbe529b5a495ced9bd159a2cd7
Narek Vardanjan <narekvardanjan@gmail.com>
2020-11-19 11:56:29 +0100

    Remove hashing

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/binary"
 	"fmt"
 	"net/http"
 	"os"
@@ -12,10 +10,8 @@ const NumberOfLogs = 1_000_000
 
 func main() {
 	fmt.Println(NumberOfLogs, " logs should be printed")
-	var b = make([]byte, 4)
 	for i := 0; i < NumberOfLogs; i++ {
-		binary.LittleEndian.PutUint32(b, uint32(i))
-		fmt.Println("Hash for the number ", i, " is ", sha1.Sum(b))
+		fmt.Println("Hash for the number ", i, " is ", i%6151)
 	}
 
 	if len(os.Args) > 1 {
