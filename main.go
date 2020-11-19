75cabbcb921b999a1b23971cd8e3d3afeac5f7d3
Narek Vardanjan <narekvardanjan@gmail.com>
2020-11-19 11:43:30 +0100

    Make server

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"crypto/sha1"
 	"encoding/binary"
 	"fmt"
+	"net/http"
+	"os"
 )
 
 const NumberOfLogs = 1_000_000
@@ -15,4 +17,15 @@ func main() {
 		binary.LittleEndian.PutUint32(b, uint32(i))
 		fmt.Println("Hash for the number ", i, " is ", sha1.Sum(b))
 	}
+
+	if len(os.Args) > 1 {
+		if os.Args[1] == "build" {
+			return
+		}
+	}
+	http.Handle("/", http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Write([]byte("OK"))
+	}))
+
+	http.ListenAndServe(":8080", nil)
 }
