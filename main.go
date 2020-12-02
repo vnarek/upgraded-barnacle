2783d258490d99b69905e2481e52084a8920020b
Narek Vardanjan <narekvardanjan@gmail.com>
2020-12-02 14:20:18 +0100

    Edit

--- main.go
+++ main.go
@@ -13,11 +13,8 @@ func main() {
 	fmt.Println(NumberOfLogs-1, " logs should be printed")
 	for i := 0; i < NumberOfLogs; i++ {
 		fmt.Println("Hash for the number ", i, " is ", i%6151)
-		if i % 1000 == 0 {
-			time.Sleep(1*time.Second)
-		}
 	}
-
+	time.Sleep(10*time.Second)
 	if len(os.Args) > 1 {
 		if os.Args[1] == "build" {
 			return
