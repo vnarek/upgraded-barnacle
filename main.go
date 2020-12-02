14b3aa812fa410569cb7317a703fd2ba4a73ecaa
Narek Vardanjan <narekvardanjan@gmail.com>
2020-12-02 13:35:04 +0100

    Add sleep

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 const NumberOfLogs = 100_000
@@ -12,6 +13,9 @@ func main() {
 	fmt.Println(NumberOfLogs-1, " logs should be printed")
 	for i := 0; i < NumberOfLogs; i++ {
 		fmt.Println("Hash for the number ", i, " is ", i%6151)
+		if i % 1000 == 0 {
+			time.Sleep(1*time.Second)
+		}
 	}
 
 	if len(os.Args) > 1 {
