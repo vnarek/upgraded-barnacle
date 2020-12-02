00ba45c23be6721ae2df4ecae4ef7a676372e1fe
Narek Vardanjan <narekvardanjan@gmail.com>
2020-12-02 14:20:18 +0100

    Edit

--- main.go
+++ main.go
@@ -4,22 +4,19 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
 )
 
-const NumberOfLogs = 100_000
+const NumberOfLogs = 1000
 
 func main() {
 	fmt.Println(NumberOfLogs-1, " logs should be printed")
 	for i := 0; i < NumberOfLogs; i++ {
 		fmt.Println("Hash for the number ", i, " is ", i%6151)
-		if i % 1000 == 0 {
-			time.Sleep(1*time.Second)
-		}
 	}
-
 	if len(os.Args) > 1 {
 		if os.Args[1] == "build" {
+			fmt.Println("some horrible stuff happened")
+			os.Exit(1)
 			return
 		}
 	}
