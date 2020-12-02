106bc51702f664b64921557d8cd96c35ae4f5517
Narek Vardanjan <narekvardanjan@gmail.com>
2020-12-02 14:20:18 +0100

    Edit

--- main.go
+++ main.go
@@ -7,19 +7,17 @@ import (
 	"time"
 )
 
-const NumberOfLogs = 100_000
+const NumberOfLogs = 2000
 
 func main() {
 	fmt.Println(NumberOfLogs-1, " logs should be printed")
 	for i := 0; i < NumberOfLogs; i++ {
 		fmt.Println("Hash for the number ", i, " is ", i%6151)
-		if i % 1000 == 0 {
-			time.Sleep(1*time.Second)
-		}
 	}
-
 	if len(os.Args) > 1 {
 		if os.Args[1] == "build" {
+			time.Sleep(10*time.Second)
+			panic("some horrible stuff happened")
 			return
 		}
 	}
